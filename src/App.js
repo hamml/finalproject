fbc77a21bcbdbe03f1ed3c0b3d18129c99397a8a
hamml <hamml@uw.edu>
2021-03-16 16:13:47 -0700

    second commit, hopefully the files will actually update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+          HCDE 438 Final Project
       </header>
+
+      <main className="center">
+        <div className="photo" />
+        <info>
+          About Me
+        </info>
+        <info className="body">
+          Hi, I'm Lyle! I'm a third-year University of Washington student studying Human Centered Design & Engineering. I'm passionate about projects involving user experience and graphic design in an inclusive and sustainable manner. Please feel free to reach out to me via email or LinkedIn to chat. 
+        </info>
+      </main>
+
+      <main className="footer">
+        <info>
+          https://www.linkedin.com/in/lshamm/ // lyle.hamm002@gmail.com
+        </info>
+      </main>
     </div>
   );
 }
